d8bf133b8b5f10560221c06b1f4ef86d768c8784
Ian Douglas <ian.douglas@iandouglas.com>
2014-06-16 14:43:30 -0600

    wip go segment

---
+++ cookbooks/sensu/providers/json_file.rb
@@ -0,0 +1,29 @@
+action :create do
+  unless Sensu::JSONFile.compare_content(new_resource.path, new_resource.content)
+    directory ::File.dirname(new_resource.path) do
+      recursive true
+      owner new_resource.owner
+      owner new_resource.group
+      mode 0750
+    end
+
+    f = file new_resource.path do
+      owner new_resource.owner
+      group new_resource.group
+      mode new_resource.mode
+      content Sensu::JSONFile.dump_json(new_resource.content)
+      notifies :create, "ruby_block[sensu_service_trigger]", :delayed
+    end
+
+    new_resource.updated_by_last_action(f.updated_by_last_action?)
+  end
+end
+
+action :delete do
+  f = file new_resource.path do
+    action :delete
+    notifies :create, "ruby_block[sensu_service_trigger]", :delayed
+  end
+
+  new_resource.updated_by_last_action(f.updated_by_last_action?)
+end
