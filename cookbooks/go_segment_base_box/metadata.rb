2da24a225b72f589071a5dd6ddec095342731849
Ian Douglas <ian.douglas@iandouglas.com>
2014-06-16 16:28:02 -0600

    wip, not working

--- cookbooks/go_segment_base_box/metadata.rb
+++ cookbooks/go_segment_base_box/metadata.rb
@@ -6,8 +6,10 @@ description      'Installs/Configures go_segment_base_box'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'db-schema', '~> 7.2'
 depends 'sendgrid_mysql', '~> 2.0'
 depends 'sendgrid_elasticsearch', '~> 0.4'
+depends 'sendgrid_application'
 depends 'golang'
 depends 'rbenv'
 depends 'java'
